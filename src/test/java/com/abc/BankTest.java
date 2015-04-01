fcdaa9647cd01da1739d175ac6d75e06bf628c7e
Gopal Shah <gopal.shah@ishisystems.com>
2015-04-01 06:49:00 -0400

    Gopal.Shah: Rev2: Refactored tests.

--- src/test/java/com/abc/BankTest.java
+++ src/test/java/com/abc/BankTest.java
@@ -1,90 +1,106 @@
 package com.abc;
 
+import org.junit.Before;
 import org.junit.Test;
+import static org.junit.Assert.assertThat;
 
 import static org.junit.Assert.assertEquals;
+import static org.hamcrest.CoreMatchers.is;
 
 public class BankTest {
-    private static final double DOUBLE_DELTA = 1e-15;
+    private Bank bank;
+
+    @Before
+    public void setupBank() {
+        bank = new Bank();
+    }
 
     @Test
     public void customerSummary() {
-        Bank bank = new Bank();
-        Customer john = new Customer("John");
-        john.openAccount(new Account(Account.CHECKING));
-        bank.addCustomer(john);
-
+        openCheckingAccountForCustomerHavingName("John");
         assertEquals("Customer Summary\n - John (1 account)", bank.customerSummary());
     }
 
     @Test
-    public void customerSummaryForCustomerWithTwoAccounts() {
-        Bank bank = new Bank();
-        Customer john = new Customer("John");
-        john.openAccount(new Account(Account.CHECKING));
-        john.openAccount(new Account(Account.SAVINGS));
-        bank.addCustomer(john);
+    public void customerSummaryForTwoCustomers() {
+        openCheckingAccountForCustomerHavingName("John");
+        openCheckingAccountForCustomerHavingName("Wayne");
+        assertEquals("Customer Summary\n - John (1 account)\n" +
+                " - Wayne (1 account)", bank.customerSummary());
+    }
 
+    @Test
+    public void customerSummaryForCustomerWithTwoAccounts() {
+        Customer john = openCheckingAccountForCustomerHavingName("John");
+        openAccount(john, Account.SAVINGS);
         assertEquals("Customer Summary\n - John (2 accounts)", bank.customerSummary());
     }
 
     @Test
     public void checkingAccountInterestEarned() {
-        Bank bank = new Bank();
-        Account checkingAccount = new Account(Account.CHECKING);
-        Customer bill = new Customer("Bill").openAccount(checkingAccount);
-        bank.addCustomer(bill);
-
-        checkingAccount.deposit(100.0);
-
-        assertEquals(0.1, bank.totalInterestPaid(), DOUBLE_DELTA);
+        int amount = 100;
+        Account account = openAccountOfType(Account.CHECKING);
+        account.deposit(amount);
+        assertThat(bank.totalInterestPaid(), is(amount * 0.001));
     }
 
     @Test
     public void savingsAccountInterestEarnedWhenAmountIsGreaterThanThousand() {
-        Bank bank = new Bank();
-        Account savingsAccount = new Account(Account.SAVINGS);
-        bank.addCustomer(new Customer("Bill").openAccount(savingsAccount));
-
-        savingsAccount.deposit(1500.0);
-
-        assertEquals(2.0, bank.totalInterestPaid(), DOUBLE_DELTA);
+        int amount = 1500;
+        Account account = openAccountOfType(Account.SAVINGS);
+        account.deposit(amount);
+        assertThat(bank.totalInterestPaid(), is(1000 * 0.001 + (amount - 1000) * 0.002));
     }
 
     @Test
     public void testSavingsAccountInterestEarnedWhenAmountIsLessThanThousand() {
-        Bank bank = new Bank();
-        Account savingsAccount = new Account(Account.SAVINGS);
-        bank.addCustomer(new Customer("Bill").openAccount(savingsAccount));
-        savingsAccount.deposit(500.0);
-        assertEquals(0.5, bank.totalInterestPaid(), DOUBLE_DELTA);
+        int amount = 500;
+        Account account = openAccountOfType(Account.SAVINGS);
+        account.deposit(amount);
+        assertThat(bank.totalInterestPaid(), is(amount * 0.001));
     }
 
     @Test
     public void testMaxiSavingsAccountInterestEarnedWhenAmountIsGreaterThanTwoThousand() {
-        Bank bank = new Bank();
-        Account maxiSavingsAccount = new Account(Account.MAXI_SAVINGS);
-        bank.addCustomer(new Customer("Bill").openAccount(maxiSavingsAccount));
-        maxiSavingsAccount.deposit(3000.0);
-        assertEquals(170.0, bank.totalInterestPaid(), DOUBLE_DELTA);
+        int amount = 3000;
+        Account account = openAccountOfType(Account.MAXI_SAVINGS);
+        account.deposit(amount);
+        assertThat(bank.totalInterestPaid(), is(1000 * 0.02 + 1000 * 0.05 + (amount - 2000) * 0.1));
     }
 
     @Test
     public void testMaxiSavingsAccountInterestEarnedWhenAmountIsLessThanTwoThousand() {
-        Bank bank = new Bank();
-        Account maxiSavingsAccount = new Account(Account.MAXI_SAVINGS);
-        bank.addCustomer(new Customer("Bill").openAccount(maxiSavingsAccount));
-        maxiSavingsAccount.deposit(1500.0);
-        assertEquals(45.0, bank.totalInterestPaid(), DOUBLE_DELTA);
+        int amount = 1500;
+        Account account = openAccountOfType(Account.MAXI_SAVINGS);
+        account.deposit(amount);
+        assertThat(bank.totalInterestPaid(), is(1000 * 0.02 + (amount - 1000) * 0.05));
     }
 
     @Test
     public void testMaxiSavingsAccountInterestEarnedWhenAmountIsLessThanOneThousand() {
-        Bank bank = new Bank();
-        Account maxiSavingsAccount = new Account(Account.MAXI_SAVINGS);
-        bank.addCustomer(new Customer("Bill").openAccount(maxiSavingsAccount));
-        maxiSavingsAccount.deposit(500.0);
-        assertEquals(10.0, bank.totalInterestPaid(), DOUBLE_DELTA);
+        int amount = 500;
+        Account account = openAccountOfType(Account.MAXI_SAVINGS);
+        account.deposit(amount);
+        assertThat(bank.totalInterestPaid(), is(amount * 0.02));
     }
 
+    private Account openAccountOfType(int accountType) {
+        Customer bill = new Customer("Bill");
+        Account account = openAccount(bill, accountType);
+        bank.addCustomer(bill);
+        return account;
+    }
+
+    private Customer openCheckingAccountForCustomerHavingName(String customerName) {
+        Customer customer = new Customer(customerName);
+        openAccount(customer, Account.CHECKING);
+        bank.addCustomer(customer);
+        return customer;
+    }
+
+    private Account openAccount(Customer customer, int accountType) {
+        Account account = new Account(accountType);
+        customer.openAccount(account);
+        return account;
+    }
 }
--- src/test/java/com/abc/CustomerTest.java
+++ src/test/java/com/abc/CustomerTest.java
@@ -1,6 +1,5 @@
 package com.abc;
 
-import org.junit.Ignore;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
@@ -8,12 +7,10 @@ import static org.junit.Assert.assertEquals;
 public class CustomerTest {
 
     @Test //Test customer statement generation
-    public void testApp(){
-
-        Account checkingAccount = new Account(Account.CHECKING);
-        Account savingsAccount = new Account(Account.SAVINGS);
-
-        Customer henry = new Customer("Henry").openAccount(checkingAccount).openAccount(savingsAccount);
+    public void testCustomerStatementForMultiAccounts(){
+        Customer henry = new Customer("Henry");
+        Account checkingAccount = openAccount(henry, Account.CHECKING);
+        Account savingsAccount = openAccount(henry, Account.SAVINGS);
 
         checkingAccount.deposit(100.0);
         savingsAccount.deposit(4000.0);
@@ -34,41 +31,27 @@ public class CustomerTest {
     }
 
     @Test
-    public void testCustomerStatementInMaxSavingsAccount() {
-        Account account = new Account(Account.MAXI_SAVINGS);
-        Customer henry = new Customer("Henry").openAccount(account);
-        account.deposit(100.0);
-        assertEquals("Statement for Henry\n" +
-                "\n" +
-                "Maxi Savings Account\n" +
-                "  deposit $100.00\n" +
-                "Total $100.00\n" +
-                "\n" +
-                "Total In All Accounts $100.00", henry.getStatement());
+    public void testCustomerStatementForCustomerHavingMaxSavingsAccount() {
+        verifyCustomerStatement(Account.MAXI_SAVINGS, "Maxi Savings Account");
     }
 
     @Test
-    public void testCustomerStatementInCheckingAccount() {
-        Account account = new Account(Account.CHECKING);
-        Customer henry = new Customer("Henry").openAccount(account);
-        account.deposit(100.0);
-        assertEquals("Statement for Henry\n" +
-                "\n" +
-                "Checking Account\n" +
-                "  deposit $100.00\n" +
-                "Total $100.00\n" +
-                "\n" +
-                "Total In All Accounts $100.00", henry.getStatement());
+    public void testCustomerStatementForCustomerHavingCheckingAccount() {
+        verifyCustomerStatement(Account.CHECKING, "Checking Account");
     }
 
     @Test
-    public void testCustomerStatementInSavingsAccount() {
-        Account account = new Account(Account.SAVINGS);
-        Customer henry = new Customer("Henry").openAccount(account);
+    public void testCustomerStatementForCustomerHavingSavingsAccount() {
+        verifyCustomerStatement(Account.SAVINGS, "Savings Account");
+    }
+
+    private void verifyCustomerStatement(int accountType, String accountNameInStatement) {
+        Customer henry = new Customer("Henry");
+        Account account = openAccount(henry, accountType);
         account.deposit(100.0);
         assertEquals("Statement for Henry\n" +
                 "\n" +
-                "Savings Account\n" +
+                accountNameInStatement + "\n" +
                 "  deposit $100.00\n" +
                 "Total $100.00\n" +
                 "\n" +
@@ -77,15 +60,22 @@ public class CustomerTest {
 
     @Test
     public void testOneAccount() {
-        Customer oscar = new Customer("Oscar").openAccount(new Account(Account.SAVINGS));
+        Customer oscar = new Customer("Oscar");
+        openAccount(oscar, Account.SAVINGS);
         assertEquals(1, oscar.getNumberOfAccounts());
     }
 
     @Test
     public void testTwoAccount() {
-        Customer oscar = new Customer("Oscar")
-                .openAccount(new Account(Account.SAVINGS));
-        oscar.openAccount(new Account(Account.CHECKING));
+        Customer oscar = new Customer("Oscar");
+        openAccount(oscar, Account.SAVINGS);
+        openAccount(oscar, Account.CHECKING);
         assertEquals(2, oscar.getNumberOfAccounts());
+    }
+
+    private Account openAccount(Customer customer, int accountType) {
+        Account account = new Account(accountType);
+        customer.openAccount(account);
+        return account;
     }
 }
